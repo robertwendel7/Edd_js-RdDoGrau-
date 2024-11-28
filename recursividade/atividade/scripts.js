7c4deb3a9d57a0f2ed825faf2776c21690529fab
RianDantas <134387076+RianDantas@users.noreply.github.com>
2024-11-28 20:18:10 +0000

    rhn

--- recursividade/atividade/scripts.js
+++ recursividade/atividade/scripts.js
@@ -15,7 +15,22 @@ function exponenciacao(n1, n2){
 
 
 }
-    
+
+let memo = []
+
+function fibo(mes){
+    if(mes === 0 || mes === 1){
+        return mes
+    }
+
+    if(memo[mes]){
+        return memo[mes]
+    }else {
+        memo[mes] = fibo(mes-1) + fibo(mes-2)
+    }
+    return memo[mes]
+}
 
 console.log("soma: " + multiplicacao(5, 5))
 console.log("soma: " + exponenciacao(5, 5))
+console.log("Coelhos: " + fibo(12))
