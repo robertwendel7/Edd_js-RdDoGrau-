508deb85050f4d48478de2345d223ef2ead1673c
Robert Wendel <134088034+robertwendel7@users.noreply.github.com>
2024-12-05 20:17:48 +0000

    primeiro commit

---
+++ ProjetoPilha/script.js
@@ -0,0 +1,69 @@
+pilha = []
+
+function mostrarPilha(){
+    document.querySelector('#resultado').innerHTML = "Resultado da pilha: " + pilha;
+}
+function mostrarTamanho(){
+    
+}
+function clickPush(){
+    valor = document.getElementById("input").value;
+    push(valor);
+    console.log(pilha);
+    mostrarPilha()
+
+}
+function clickPop(){
+    
+    pop();
+    console.log(pilha);
+    mostrarPilha()
+}
+function clickPeek(){
+    
+    peek();
+    mostrarPilha()
+    
+}
+function clickIsEmpty(){
+   
+    isEmpty();
+    mostrarPilha()
+
+    
+}
+function clickSize(){
+    
+    size();
+    mostrarPilha()
+    
+}
+
+
+function push(valor){
+    pilha.push(valor);
+    
+}
+
+function pop(){
+    pilha.pop();
+
+}
+
+function peek(){
+    return pilha[pilha.length - 1];
+
+}
+
+function isEmpty(){
+    return size() === 0
+
+}
+
+function size(){
+    return pilha.length
+
+}
+
+
+document.querySelector
